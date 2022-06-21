e1526c6af0231aa2622bf2827724513f7b1df881
Sheena Bonilla <sheena.myb@gmail.com>
2022-06-21 09:03:24 -0700

    Create photo class

---
+++ index.js
@@ -0,0 +1,7 @@
+const base_url = "http://127.0.0.1:3000"
+
+function createForm() {
+    debugger
+    // let photoForm = document.createElementById("photo-form")
+    // debugger
+}
---
+++ src/photo.js
@@ -0,0 +1,9 @@
+class Photo {
+    constructor(img_src, caption, user_id) {
+        this.img_src = img_src
+        this.caption = caption
+        this.user_id = user_id
+    }
+
+    
+}
